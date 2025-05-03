4d89362d4a48c5078254599a6858253bbc1a43c9
seflosorio <sergiofosorio@utexas.edu>
2025-05-03 17:50:18 -0500

    added bubble respawn functionality

--- src/App.js
+++ src/App.js
@@ -4,28 +4,57 @@ import {useState, useEffect} from 'react';
 
 function Bubble() {
   const [isPopped, setIsPopped] = useState(false);
+  const [timedOut, setTimedOut] = useState(false);
 
   function handleClick() {
     setIsPopped(true);
   };
 
+  // tracks how long bubble has been on screen
+  // can edit 3000 to change time-out time.
   useEffect(() => {
-    let timer;
+    console.log("First useEffect running. isPopped:", isPopped, "timedOut:", timedOut);
 
-    if (isPopped) {
-      timer = setTimeout(() => {
+    let lifeTimer;
+
+    if (!isPopped && !timedOut) {
+      console.log("Setting life timer");
+      lifeTimer = setTimeout(() => {
+        console.log("Timer expired! Setting timedOut to true");
+        setTimedOut(true);
+      }, 3000)
+    }
+  
+    // clean up timer
+    return () => {
+      if (lifeTimer) {
+        clearTimeout(lifeTimer);
+      }
+    }
+  }, [isPopped, timedOut])
+
+  useEffect(() => {
+    console.log("Second useEffect running. isPopped:", isPopped, "timedOut:", timedOut);
+    let respawnTimer;
+    if (isPopped || timedOut) {
+      console.log("Setting respawn timer");
+      respawnTimer = setTimeout(() => {
+        console.log("Respawn timer expired! Resetting states");
         setIsPopped(false);
-      }, 2000);
+        setTimedOut(false);
+        // bubble will generate in random 2 sec interval, edit 2000 to change interval (1k is 1 second)
+      }, Math.floor(Math.random() * 2000) + 1);
     }
 
     return () => {
-      if (timer) {
-        clearTimeout(timer);
+      if (respawnTimer) {
+        console.log("Clearing respawn timer");
+        clearTimeout(respawnTimer);
       }
     };
-  }, [isPopped])
+  }, [isPopped, timedOut])
 
-  return <button className={isPopped ? 'popped' : 'bubble'} onClick={handleClick} disabled={isPopped}></button>
+  return <button className={isPopped ? 'popped' : timedOut ? 'timed-out' : 'bubble'} onClick={handleClick} disabled={isPopped || timedOut}></button>
 }
 
 function Game() {
