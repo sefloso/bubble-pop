45eb71b36aef4d9d1c660a3fc8be6535d4b96bc6
seflosorio <sergiofosorio@utexas.edu>
2025-05-15 18:04:19 -0500

    HomePage.js cleanup

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,10 @@
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from './bubbleLogo.png';
 import './HomePage.css';
 import Header from '../Header/Header';
 
 
 function HomePage() {
-    const navigate = useNavigate();
     return (
         <div className="home-container">
             <Header />
